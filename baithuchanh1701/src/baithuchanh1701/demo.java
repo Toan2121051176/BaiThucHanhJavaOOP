537309467218c41252e908a24ccdea69e6d81047
unknown <toansd9a@gmail.com>
2024-01-23 18:15:35 +0700

    Bài thực hành 2 & slide 92

---
+++ baithuchanh1701/src/baithuchanh1701/demo.java
@@ -0,0 +1,13 @@
+package baithuchanh1701;
+
+import java.util.Scanner;
+
+public class demo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập vào tên của bạn:");
+		String name = sc.nextLine();
+		System.out.println("xin chào bạn:"+name);
+		
+	}
+}
