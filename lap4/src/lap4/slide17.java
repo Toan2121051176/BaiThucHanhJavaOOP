933dbe747a1164f9b42ffa6f7a2dc1031c23fb47
unknown <toansd9a@gmail.com>
2024-05-08 17:28:52 +0700

    vong lap for

---
+++ lap4/src/lap4/slide17.java
@@ -0,0 +1,17 @@
+package lap4;
+
+public class slide17 {
+	public static void main(String[] args) {
+		ArrayList<String> arrListString = new ArrayList<>();
+		ArrListString.add("JAVA");
+		ArrListString.add("PHP");
+		ArrListString.add("C#");
+		ArrListString.add("C++");
+		System.out.print("Cac phan tu co trong ArrListString la :");
+		for (int i = 0; i< arrListString.size(); i++) {
+			System.out.print(arrListString.get(i)+ "\t");
+		}
+		
+	}
+
+}
