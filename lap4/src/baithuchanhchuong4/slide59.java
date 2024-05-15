f8ba9d22728df483ae0c447cc299ab2540a180e1
unknown <toansd9a@gmail.com>
2024-05-15 16:38:36 +0700

    slide 59

--- lap4/src/baithuchanhchuong4/slide59.java
+++ lap4/src/baithuchanhchuong4/slide59.java
@@ -9,12 +9,12 @@ import java.util.Scanner;
         lhs.add("Chu");
         lhs.add("The");
         lhs.add("Toan");
-        System.out.println("Nhập 1 phần tử cần xóa :");
+        System.out.print("Nhập 1 phần tử cần xóa :");
         String number = sc.nextLine();
         if(lhs.contains(number)) {
             lhs.remove(number);
-            System.out.println("dãy sau khi xóa:");
-            System.out.println(lhs);
+            System.out.print("dãy sau khi xóa:");
+            System.out.print(lhs);
         }else
             System.out.printf("Không tồn tại %s trong dãy",number);
         
