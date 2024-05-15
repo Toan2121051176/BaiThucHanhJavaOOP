fe21e62264cf77d27386fdccda2a8634ef0cf4c4
unknown <toansd9a@gmail.com>
2024-05-15 13:12:16 +0700

    slide 25

---
+++ lap4/src/baithuchanhchuong4/slide25.java
@@ -0,0 +1,34 @@
+package baithuchanhchuong4;
+import java.util.ArrayList; 
+import java.util.Scanner;
+
+public class slide25 {
+
+	public class Max {
+
+	public static void main(String[] args) {
+
+	ArrayList<Integer> arrListInteger = new ArrayList<>(); Scanner scanner = new Scanner(System.in);
+
+	int number; 
+
+	System.out.println("Nhập số phân tử của ArrayList: ");
+
+	int n scanner.nextInt();
+
+
+
+	System.out.print("Nhập phân tử thứ " + i + ": ");
+
+	number scanner.nextInt(); arrListInteger.add(number);
+
+	}
+
+	for (int i = 1; i < arrListInteger.size(); i++) { 
+	if (arrListInteger.get(i).compareTo(max) > 0) {
+
+	max arrListInteger.get(i);
+
+	} System.out.println("Phần tử lớn nhất trong arrListInteger max);
+
+}
