8f6d3fbb70f6ee8d1ada300e820e553dc3bf3d40
unknown <toansd9a@gmail.com>
2024-05-15 13:15:45 +0700

    slide 27

---
+++ lap4/src/baithuchanhchuong4/slide27.java
@@ -0,0 +1,21 @@
+package baithuchanhchuong4;
+import java.util.LinkedListList;
+
+public class slide27 {
+
+public class NameClass (
+
+public static void main(String[] args) {
+
+
+LinkedList<Interger> linkedList = new LinkedList<>(); }
+public class NameClass {
+	
+
+public static void main(String[] args) {
+	LinkedList list = new LinkedList<>();
+}
+
+}
+
+}
