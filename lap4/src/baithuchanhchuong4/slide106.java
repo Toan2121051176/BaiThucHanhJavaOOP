b71fe3e1d01dc67c4e21124f7b107d04a453930d
unknown <toansd9a@gmail.com>
2024-05-22 14:39:29 +0700

    fix slide 106

--- lap4/src/baithuchanhchuong4/slide106.java
+++ lap4/src/baithuchanhchuong4/slide106.java
@@ -1,33 +1,25 @@
 package baithuchanhchuong4;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class slide106 {
     public static void main(String[] args) {
 
+        TreeMap<Integer, Character> treeMap = new TreeMap<>();
 
-TreeMap<Integer, Character treeMap = new TreeMap<>();
+        treeMap.put(6, 'A');
+        treeMap.put(5, 'B');
+        treeMap.put(1, 'C');
+        treeMap.put(2, 'D');
+        treeMap.put(8, 'E');
 
+        Set<Entry<Integer, Character>> setTreeMap = treeMap.entrySet();
 
-treeMap.put(6, 'A');
-
-treeMap.put(5, 'B');
-
-treeMap.put(1, 'C');
-
-treeMap.put(2, 'D');
-
-treeMap.put(8, 'E);
-
-
-
-Set<Entry<Integer, Character>> setTreeMap treeMap.entrySet();
-
-
-
-}
-
-    
+        for (Map.Entry<Integer, Character> entry : setTreeMap) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
 }
