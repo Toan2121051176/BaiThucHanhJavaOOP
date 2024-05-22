f696f3d9bdc9fdf0b5151ab5b1b5666231579e0c
unknown <toansd9a@gmail.com>
2024-05-22 14:45:23 +0700

    fix

--- lap4/src/baithuchanhchuong4/TeenClass.java
+++ lap4/src/baithuchanhchuong4/TeenClass.java
@@ -1,8 +1,9 @@
 package baithuchanhchuong4;
 
-public class TeenClass {
-	public static void main(String[] args) {
-		ArrayList<String> arrListString = new ArrayList<>(10);
-	}
+import java.util.ArrayList;
 
+public class TeenClass {
+    public static void main(String[] args) {
+        ArrayList<String> arrListString = new ArrayList<>();
+    }
 }
